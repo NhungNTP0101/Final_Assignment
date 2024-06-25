2197e6ead50c20ffc042307b95e97ac1cd429cf8
NhungNTP0101 <nguyenphuongnhung0101@gmail.com>
2024-06-25 22:34:12 +0700

    Update Excel file

--- src/main/java/com/vmo/demoWebShop/utils/ExcelConfig.java
+++ src/main/java/com/vmo/demoWebShop/utils/ExcelConfig.java
@@ -29,7 +29,7 @@ public class ExcelConfig {
 
     public ExcelConfig() {
         try {
-            File file = new File("F:\\Mine\\Automation\\assignment.xlsx");
+            File file = new File("src/main/java/com/vmo/demoWebShop/utils/assignment.xlsx");
             fileIn = new FileInputStream(file);
             wb = new XSSFWorkbook(fileIn);
         } catch (Exception e) {
